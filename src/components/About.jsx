eaf14302be6b78cd2a833a6e2dc1a7de208148f0
Manasa S S <manasasridhar2000@gmail.com>
2026-06-11 05:53:42 +0530

    Added about component

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+
+      <p>
+        I am Manasa S S, an MCA graduate and Software Developer with
+        experience in developing web applications using ASP.NET,
+        Django, JavaScript, HTML, CSS, and MySQL.
+      </p>
+
+      <p>
+        I enjoy building user-friendly applications and continuously
+        learning new technologies. Currently, I am exploring
+        Artificial Intelligence and Machine Learning to expand my
+        technical expertise.
+      </p>
+    </section>
+  );
+}
+
+export default About;
