d438743f2f399bd8c20b8a6b4fcc1f48965726ea
Manasa S S <manasasridhar2000@gmail.com>
2026-06-11 06:01:02 +0530

    Skills component added

---
+++ src/components/Skills.jsx
@@ -0,0 +1,28 @@
+function Skills(){
+  return (
+    <>
+    <section>
+      <h2>Skills</h2>
+
+      <div>
+        <h3>Frontend</h3>
+        <p>HTML, CSS, JavaScript, React</p>
+
+        <h3>Backend</h3>
+        <p>Python, Django, ASP.NET, ASP.NET Core MVC</p>
+
+        <h3>Database</h3>
+        <p>MySQL, SQL Server</p>
+
+        <h3>Tools</h3>
+        <p>Git, GitHub, VS Code, Visual Studio</p>
+
+        <h3>Currently Learning</h3>
+        <p>Artificial Intelligence, Machine Learning</p>
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default Skills;
