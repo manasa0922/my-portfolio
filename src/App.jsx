3374f6cdf9fa4adf112eaf3152b0094d56eaac3b
Manasa S S <manasasridhar2000@gmail.com>
2026-06-11 05:32:46 +0530

    React portfolio initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Manasa S S</h1>
+      <h2>MCA Graduate</h2>
+      <h3>Software Developer</h3>
+
+      <p>
+        Passionate about building web applications using ASP.NET, Django,
+        JavaScript and currently learning Artificial Intelligence and Machine Learning.
+      </p>
+    </>
+  );
+}
+
+export default App;
